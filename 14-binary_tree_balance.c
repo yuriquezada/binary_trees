40d52f2020cb11e80d2292b7ab8b2b840520650a
Rayd Trujillo <rayd1893@gmail.com>
2021-12-02 20:30:34 +0000

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
  */
 int height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		int l = 0, r = 0;
+        size_t left, right;
 
-		l = tree->left ? 1 + height(tree->left) : 0;
-		r = tree->right ? 1 + height(tree->right) : 0;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+	if (tree == NULL)
+		return (0);
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
 }
 
 /**
@@ -28,19 +28,12 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance = 0;
 	int lh, rh;
 
-	if (tree->left == NULL)
-		lh = 0;
-	else
-		lh = 1 + height(tree->left);
-	if (tree->right == NULL)
-		rh = 0;
-	else
-		rh = 1 + height(tree->right);
+	if (tree == NULL)
+		return (0);
+	lh = height(tree->left);
+	rh = height(tree->right);
 
-	balance = lh - rh;
-
-	return (balance);
+	return (lh - rh);
 }
