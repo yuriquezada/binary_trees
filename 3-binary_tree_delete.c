e499202578ebc643c65da71fac4bb4fa9e307750
Rayd Trujillo <rayd1893@gmail.com>
2021-12-02 02:57:18 +0000

    Add betty style to function

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,4 +1,11 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Delete tree
+ * @tree: tree
+ *
+ * Return: Nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
